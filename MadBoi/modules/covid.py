feab266d6bd02aee4e22300ce46d9a0ed2dd070c
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-03-11 17:07:36 +0530

    MadBoi

--- MadBoi/modules/covid.py
+++ MadBoi/modules/covid.py
@@ -12,11 +12,11 @@ def covid(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Global Totals** 🦠\n\n__Cases:__ {r['cases']:,}\n__Cases Today:__ {r['todayCases']:,}\n__Deaths:__ {r['deaths']:,}\n__Deaths Today:__ {r['todayDeaths']:,}\n__Recovered:__ {r['recovered']:,}\n__Active:__ {r['active']:,}\n__Critical:__ {r['critical']:,}\n__Cases/Mil:__ {r['casesPerOneMillion']}\n__Deaths/Mil:__ {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Cases for {r['country']} 🦠**\n\n__Cases:__ {r['cases']:,}\n__Cases Today:__ {r['todayCases']:,}\n__Deaths:__ {r['deaths']:,}\n__Deaths Today:__ {r['todayDeaths']:,}\n__Recovered:__ {r['recovered']:,}\n__Active:__ {r['active']:,}\n__Critical:__ {r['critical']:,}\n__Cases/Mil:__ {r['casesPerOneMillion']}\n__Deaths/Mil:__ {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 

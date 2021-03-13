101598c37aa1854d384534d383755fa4251d6dc7
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-03-13 15:23:41 +0530

    MadBoi

--- MadBoy/__init__.py
+++ MadBoy/__init__.py
@@ -1,3 +1,18 @@
 from MadBoi.events import register as MadBoii
 from MadBoi import telethn as bot
-from MadBoi import *
+from MadBoi import API_ID, API_HASH
+from MadBoi.events import *
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+import os
+STRING_SESSION = os.environ.get("STRING_SESSION")
+if STRING_SESSION:
+    user = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+else:
+     print ("please add StringSession var")
+
+try:
+     user.start()
+except Exception as e:
+     print(e)

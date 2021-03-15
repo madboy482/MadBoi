b0afc593a555793f2b3a98a566da904fd9c52d24
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-03-15 14:27:23 +0530

    MadBoi

--- MadBoi/modules/connection.py
+++ MadBoi/modules/connection.py
@@ -419,3 +419,5 @@ dispatcher.add_handler(DISCONNECT_CHAT_HANDLER)
 dispatcher.add_handler(ALLOW_CONNECTIONS_HANDLER)
 dispatcher.add_handler(HELP_CONNECT_CHAT_HANDLER)
 dispatcher.add_handler(CONNECT_BTN_HANDLER)
+
+# ROMEOv3
